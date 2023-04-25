b0fbac0c497b47f2b112fa07a4e1d2e37f5add1c
ccadjac <chen1230405@163.com>
2023-04-25 17:58:18 +0800

    权限管理2.1

--- src/router/modules/hospitalGuide.js
+++ src/router/modules/hospitalGuide.js
@@ -2,5 +2,5 @@ export default {
   path: 'hospitalGuide',
   component: () => import('@/views/hospitalGuide/index'),
   name: 'hospitalGuide',
-  meta: { title: '医院导览', icon: 'form' }
+  meta: { title: '医院导览' }
 }
